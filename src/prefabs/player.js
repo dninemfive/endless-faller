e830d726932119c619639d8d4d3ce0511c30887a
dninemfive <11084010+dninemfive@users.noreply.github.com>
2021-04-29 10:06:52 -0700

    clear out more stuff

--- src/prefabs/player.js
+++ src/prefabs/player.js
@@ -1,46 +1,19 @@
 class Player extends Phaser.GameObjects.Sprite{
     constructor(scene, x, y, texture, frame){
         super(scene, x, y, texture, frame);
-        // add an instance to scene
         scene.add.existing(this);
         this.moveSpeed = 2;
         this.hp = 10; // can be whatever
     }
     
-    update(){
-        // movement
-        let mouse = this.scene.input.activePointer;
-        if(useMouse) {
-            // https://photonstorm.github.io/phaser3-docs/Phaser.Input.Pointer.html
-            if(mouse.x < this.x && this.x >= borderUISize + this.width) {
-                this.x -= this.moveSpeed;
-            } else if(mouse.x > this.x && this.x <= game.config.width - borderUISize - this.width) {
-                this.x += this.moveSpeed;
-            }
-        } else { 
-            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
-                this.x -= this.moveSpeed;
-            } else if(keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
-                this.x += this.moveSpeed;
-            }
-            if(Phaser.Input.Keyboard.JustDown(keyF)){
-                this.fire();
-            }
+    update(){        
+        if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
+            this.x -= this.moveSpeed;
+        } else if(keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
+            this.x += this.moveSpeed;
         }
-        if(this.y <= borderUISize * 3 + borderPadding){
-            this.reset();
+        if(Phaser.Input.Keyboard.JustDown(keyF)){
+            this.fire();
         }
-    }
-
-    reset(){
-        this.isFiring = false;
-        this.y = game.config.height - borderUISize - borderPadding;
-    }
-
-    fire(){
-        if(!this.scene.gameOver && !this.isFiring){
-            this.isFiring = true;
-            this.sfxRocket.play();
-        }        
     }
 }
--- src/scenes/play.js
+++ src/scenes/play.js
@@ -40,58 +40,15 @@ class Play extends Phaser.Scene {
         this.starfield.tilePositionY -= 4;
     }
 
-    checkCollision(rocket, ship) {
+    checkCollision(player, obstacle) {
         // simple AABB checking
-        if (rocket.x < ship.x + ship.width && 
-            rocket.x + rocket.width > ship.x && 
-            rocket.y < ship.y + ship.height &&
-            rocket.height + rocket.y > ship. y) {
+        if (player.x < obstacle.x + obstacle.width && 
+            player.x + player.width > obstacle.x && 
+            player.y < obstacle.y + obstacle.height &&
+            player.height + player.y > obstacle. y) {
                 return true;
         } else {
             return false;
-        }
-    }
-
-    shipExplode(ship) {
-        // temporarily hide ship
-        ship.alpha = 0;
-        // create explosion sprite at ship's position
-        let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
-        boom.anims.play('explode');             // play explode animation
-        boom.on('animationcomplete', () => {    // callback after anim completes
-            ship.reset();                         // reset ship position        
-            ship.active = false;  
-            boom.destroy();                       // remove explosion sprite
-            this.time.delayedCall(Phaser.Math.Between(125, 375) * Math.log10(ship.points), () => {
-                ship.active = true;
-                ship.alpha = 1;                       // make ship visible again
-            }, null, this);
-        }); 
-        this.score += ship.points;
-        this.scoreLabel.text = this.score;
-        this.sound.play('sfx_explosion');
-    }
-
-    // Called once per second. Schedules a call to itself until the game is over.
-    timerTick(){
-        if (this.timeRemaining > 0) {
-            this.timeRemaining -= timerDelta;
-            this.timerLabel.text = Math.round(10 * this.timeRemaining) / 10; // round to one decimal place
-        }
-        if (this.timeRemaining <= 0){
-            this.timerLabel.text = 0;
-            if(this.score > highScore) {
-                highScore = this.score;
-            }
-            this.add.text(game.config.width / 2, game.config.height / 2 - 32, "GAME OVER", textConfig).setOrigin(0.5);
-            this.add.text(game.config.width / 2, game.config.height / 2, "High Score: " + highScore, textConfig).setOrigin(0.5);
-            this.add.text(game.config.width / 2, game.config.height / 2 + 32, "Press (R) to Restart or ← for Menu", textConfig).setOrigin(0.5);
-            this.gameOver = true;            
-            console.log("Game over. High score: " + highScore);
-        } else {            
-            this.clock = this.time.delayedCall(1000 * timerDelta, () => {
-                this.timerTick();
-            }, null, this);
         }
     }
 }
