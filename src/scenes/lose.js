7d6a1c14bb5b4a956b665cfd0675e41d937818df
dninemfive <11084010+dninemfive@users.noreply.github.com>
2021-05-03 10:28:36 -0700

    high score stuff fades in

--- src/scenes/lose.js
+++ src/scenes/lose.js
@@ -10,16 +10,31 @@ class Lose extends Phaser.Scene {
     create(){
         //this.background = this.add.sprite(game.config.width / 2, 0,"background").setOrigin(0.5,0);
         //this.background.setScale(game.config.width / this.background.width);
-        this.add.text(game.config.width / 2, (game.config.height / 2) - (borderPadding * 3), "You are dead.", textConfigWhite).setOrigin(0.5);
-        this.add.text(game.config.width / 2, game.config.height / 2, "Your score was " + score + ".", textConfigWhite).setOrigin(0.5);
-        this.add.text(game.config.width / 2, (game.config.height / 2) + (borderPadding * 3), "The high score is " + highScore, textConfigWhite).setOrigin(0.5);
-        this.add.text(game.config.width / 2, game.config.height - (borderUISize + borderPadding), "press R to restart or SPACE to return to menu", textConfigWhite).setOrigin(0.5);
+        this.youredead = this.add.text(game.config.width / 2, (game.config.height / 2) - (borderPadding * 3), "You are dead.", textConfigWhite).setOrigin(0.5);
+        this.youredead.alpha = 0;
+        this.yourscore = this.add.text(game.config.width / 2, game.config.height / 2, "Your score was " + score + ".", textConfigWhite).setOrigin(0.5);
+        this.yourscore.alpha = 0;
+        this.highscore = this.add.text(game.config.width / 2, (game.config.height / 2) + (borderPadding * 3), "The high score is " + highScore + ".", textConfigWhite).setOrigin(0.5);
+        this.highscore.alpha = 0;
+        this.returntext = this.add.text(game.config.width / 2, 
+            game.config.height - (borderUISize + borderPadding), "press R to restart or SPACE to return to menu", textConfigWhite).setOrigin(0.5);
+        this.returntext.alpha = 0;
         // might want to make this any key eventually
         this.keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
     }
 
     update(){
+        let alphaIncrease = 0.005;
+        if(this.youredead.alpha < 1){
+            this.youredead.alpha += alphaIncrease;
+        } else if(this.yourscore.alpha < 1){
+            this.yourscore.alpha += alphaIncrease;
+        } else if(this.highscore.alpha < 1){
+            this.highscore.alpha += alphaIncrease;
+        } else {
+            this.returntext.alpha = 1;
+        }
         if (Phaser.Input.Keyboard.JustDown(this.keySPACE)){
             state = STATES.MAIN;
             this.scene.start("play");
