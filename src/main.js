1ad6bf1f49326d77e6f5bf49114f3afb89543850
dninemfive <11084010+dninemfive@users.noreply.github.com>
2021-05-05 05:28:20 -0700

    finish submission comment

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 /*
     HORIZON DOWN
-    By: Graydon, Lucas, 
+    By: Graydon, Lucas, Vika
     Completed on 3 May 2021
 
     Creative tilt:
@@ -10,6 +10,15 @@
             Both the main menu and the play screen are in the same Scene class in order to make the animated transition between one and the other smoother, and a JS object
             was used to simulate enums from other languages in order to control game state (main.js l29-34, play.js l75-85, &c). I'm proud of the relatively elegant way
             these particular systems are handled, though the code could definitely be way more readable if refactored.
+        Artistic:
+                The music and the art have pieced together a theme that we did not originally intend to create, but the decision to have a set music style and lore tremendously
+            helped generate a fun world for players to experience. As the artist, I am particularly proud of the second level assets of the Core interior, as I believe the
+            second level to be a nice contrast to the brightness of the first.
+                The music was meant to evoke a driving intensity to add interest and a sensation of levity to the game. Somewhere between spy thriller and coffee house, the
+            jazz-rock score is fully composed original music, made in the open source Musescore 3. It's about 72 measures long and features five voices: Trumpet, Tenor Sax,
+            Trombone, Bass, and Drums. It was heavily inspired by the work of Gordon Goodwin's Big Phat Band, particularly their smash hit "The Jazz Police," which, if you 
+            listen to carefully, you can hear that I lifted two bars of melody from. Creating the music was a surprisingly intense process that took a lot out of me, but I am 
+            proud to have officially written my first game score, even if it is small and... let's say 'unnecessarily dissonant.'
 
 */
 let config = {
